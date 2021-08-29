4c880b70eee1d583cc0a755861194ac0463ddd34
codlipa <codlipa@gmail.com>
2021-08-29 11:58:16 +0300

    create hamburger-menu

---
+++ code/011/education/app/js/script.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelector('#hamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+hamburger.addEventListener('click', function(){
+    console.log('click hamburger');
+
+    if(header.classList.contains('open')){ // Close Hamburger Menu
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+    }
+    else { // Open Hamburger Menu
+        body.classList.add('noscroll');
+        header.classList.add('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+    }
+});
