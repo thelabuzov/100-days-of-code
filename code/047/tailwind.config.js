ac5a7e80932684b47974ba203710647896d0bfb6
codlipa <codlipa@gmail.com>
2021-10-16 03:19:22 -0700

    create tailwind netflix clone header & hero sections

---
+++ code/047/tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  purge: {
+    mode: 'layers',
+    content: [
+      'src/**/*.js',
+      'src/**/*.jsx',
+      'public/**/*.html'
+    ]
+  },
+  darkMode: false,
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('https://assets.nflxext.com/ffe/siteui/vlv3/68c9706b-acd1-4472-bb1d-ef3ca933154c/298a5fdb-322c-44cb-839c-0b144a4b7159/US-en-20211011-popsignuptwoweeks-perspective_alpha_website_large.jpg')",
+      },
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        lg: '750px',
+        xl: '750px',
+        '2xl': '750px',
+      },
+    },
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
